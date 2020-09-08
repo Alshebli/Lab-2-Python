7fff32630bc4e47ea9cce288022fd6bf45abb276
Alshebli1 <saa6016@psu.edu>
2020-09-08 16:07:27 +0000

    edited

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ def getLetterGrade(grad):
     return 'A'
   elif grad >= 90.0:
     return 'A-'
-  elif grad >= 87.0):
+  elif grad >= 87.0:
     return 'B+'
-  elif (grad >= 83.0):
-   return 'B'
-  elif (grad >= 80.0):
-   return 'B-'
-  elif (grad >= 77.0):
-   return 'C+'
-  elif (grad >= 70.0):
-   return 'C'
-  elif (grad >= 60.0):
-   return 'D'
-  elif (grad < 60.0):
-   return 'F':
+  elif grad >= 83.0:
+    return 'B'
+  elif grad >= 80.0:
+    return 'B-'
+  elif grad >= 77.0:
+    return 'C+'
+  elif grad >= 70.0:
+    return 'C'
+  elif grad >= 60.0:
+    return 'D'
+  elif grad < 60.0:
+    return 'F'
 def run():
   grad = float(input("Enter your CMPSC 131 garde: "))
   print(f"Your letter grade for CMPSC 131 is {getLetterGrade(grad)}.")
